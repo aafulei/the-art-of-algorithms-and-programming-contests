d4fc37cfb7f0b7210085517ba2e6d5e94550ab06
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-04-11 22:51:48 +0800

    Update limits.c

--- 01-getting-started/questions/limits.c
+++ 01-getting-started/questions/limits.c
@@ -5,9 +5,12 @@
 
 int main()
 {
-    printf("INT_MIN = %d\n", INT_MIN);      // -2147483648
-    printf("INT_MAX = %d\n", INT_MAX);      //  2147483647
-    printf("PI = %.100lf\n", acos(-1.0));   // 3.141592653589793115997963468544185161590576171875000... (48 digits)
-    printf("DBL_MIN = %lg\n", DBL_MIN);     // 2.22507e-308
-    printf("DBL_MAX = %lg\n", DBL_MAX);     // 1.79769e+308
+    printf("INT_MIN = %d\n", INT_MIN);              // -2147483648
+    printf("INT_MAX = %d\n", INT_MAX);              //  2147483647
+    printf("DBL_EPSILON = %lg\n", DBL_EPSILON);     // 2.22045e-16 (that 1 and 1-epsilon differ)
+    printf("DBL_MANT_DIG = %d\n", DBL_MANT_DIG);    // 53 (signicand precision)
+    printf("DBL_DIG = %d\n", DBL_DIG);              // 15 (text -> double --> text)
+    printf("DECIMAL_DIG = %d\n", DECIMAL_DIG);      // 21 (double --> text --> double)
+    printf("DBL_MIN = %lg\n", DBL_MIN);             // 2.22507e-308
+    printf("DBL_MAX = %lg\n", DBL_MAX);             // 1.79769e+308
 }
