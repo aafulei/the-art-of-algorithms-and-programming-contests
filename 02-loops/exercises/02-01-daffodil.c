ec7ced7b6328abfb544fc9338e34757853296cb7
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-04-25 16:57:40 +0800

    add

--- 02-loops/exercises/02-01-daffodil.c
+++ 02-loops/exercises/02-01-daffodil.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#define CUBE(x) ((x) * (x) * (x))
 
 int main()
 {
-    for (int i = 100, a, b, c; i <= 999; i++) {
-        a = i / 100;
-        c = i % 10;
-        b = (i - a * 100 - c) / 10;
-        if (a*a*a + b*b*b + c*c*c == i)
-            printf("%d\n", i);
-    }
-    return 0;
+  for (int i = 100, a, b, c, n; i <= 999; ++i) {
+    n = i;
+    a = n / 100;
+    n -= 100 * a;
+    b = n / 10;
+    n -= 10 * b;
+    c = n;
+    if (a * a * a + b * b * b + c * c * c == i)
+      printf("%d\n", i);
+  }
+  return 0;
 }
--- 02-loops/exercises/02-02-hanxin.c
+++ 02-loops/exercises/02-02-hanxin.c
@@ -5,7 +5,7 @@ int main()
   int a, b, c, found;
   while (scanf("%d%d%d", &a, &b, &c) == 3) {
     found = 0;
-    for (int n = 10; n < 100; n++) {
+    for (int n = 10; n < 100; ++n) {
       if (n % 3 == a && n % 5 == b && n % 7 == c) {
         printf("%d\n", n);
         found = 1;
